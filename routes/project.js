9d3c959071808b0496c5f5c97f2e301962e0c066
vagrant <vagrant@precise32.(none)>
2014-02-20 19:00:31 +0000

    added database functionality

--- routes/project.js
+++ routes/project.js
@@ -5,6 +5,10 @@ exports.projectInfo = function(req, res) { 
 
   // query for the specific project and
   // call the following callback
+models.Project
+  .find({"_id": projectID})
+  .exec(afterQuery);
+
 
   function afterQuery(err, projects) {
     if(err) console.log(err);
@@ -14,15 +18,39 @@ exports.projectInfo = function(req, res) { 
 
 exports.addProject = function(req, res) {
   var form_data = req.body;
+  var newPost = new models.Project({
+    "title":form_data["project_title"],
+    "date":form_data["date"],
+    "summary":form_data["summary"],
+    "image":form_data["image_url"]
+  });
   console.log(form_data);
+  newPost.save(afterSaving);
 
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
-}
+
+ function afterSaving(err){
+  if(err){console.log(err);res.send(500);}
+  res.send();
+  res.redirect('/'); 
+ }
+  }
+
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
 
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
+models.Project
+.find({"_id":projectID})
+.remove()
+.exec(afterRemoving);
+
+function afterRemoving(err){
+ if(err) console.log(err);
+  res.send();
+}
+
 }
